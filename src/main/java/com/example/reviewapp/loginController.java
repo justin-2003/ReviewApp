5aa25cdadb875eab223ea6f426e24a62af397db6
Justin  Sam <justinsam02544@gmail.com>
2023-10-26 23:02:07 -0400

    Made to load the primary page when you click on the login button.

--- src/main/java/com/example/reviewapp/loginController.java
+++ src/main/java/com/example/reviewapp/loginController.java
@@ -2,12 +2,24 @@ package com.example.reviewapp;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 public class loginController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     private Label loginlabel;
     @FXML
@@ -19,9 +31,14 @@ public class loginController {
     @FXML
     private Button cancel_btn;
 
-    public void onLoginButtonClick(ActionEvent e){
+    public void onLoginButtonClick(ActionEvent e) throws IOException {
         if((username_tf.getText().isEmpty() == false)&&(password_tf.getText().isEmpty()==false)){
             loginlabel.setText("Login Successful!");
+            Parent root = FXMLLoader.load(getClass().getResource("secondary.fxml"));
+            stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         }
         else{
             loginlabel.setText("Please enter valid username and password");
